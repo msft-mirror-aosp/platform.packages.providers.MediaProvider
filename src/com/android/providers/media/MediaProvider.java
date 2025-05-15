e599e8d934f2f06968cf74ca74a174b26ee34330
darshilshah <darshilshah@google.com>
2025-05-15 14:45:41 +0000

    [security-aosp-udc-staging] Remove default ignoreable characters before regex checks
    This is a combined CL for cherry-pick. Original CLs merged in main-
    ag/33577707
    ag/33920560
    ag/33920560
    
    Lower file system ignores certain characters while saving files.
    Malicious apps use these ignorable characters to by-pass regex checks
    that are needed to protect private app directories. We remove these
    characters before regex checks so that apps cannot by-pass the checks.
    
    Bug: 377888957
    Test: atest com.android.providers.media.util.FileUtilsTest#testNormalizeAndFilterDefaultIgnorableCodepoints
    Test: atest com.android.providers.media.util.FileUtilsTest#testIsDataOrObbRelativePathWithZwsChars_flagEnabled
    Test: atest com.android.providers.media.util.FileUtilsTest#testIsDataOrObbRelativePathWithZwsChars_flagDisabled
    Flag: com.android.providers.media.flags.enable_path_sanitization
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ac3a6dc8db818a74357af1dfa2acfa911648827c)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:2ce2b01a309edcd4e28460225dc5f2cc36d6597c)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ef2fb51df1261039fd461e670b225682bc365c17)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:f375a139df1e8a75319be7d722e40af9a7c2396b
    Merged-In: Ibb96bda4b4fdb0869209aeb37260bf656d155a9e
    Change-Id: Ibb96bda4b4fdb0869209aeb37260bf656d155a9e

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -8998,7 +8998,7 @@ public class MediaProvider extends ContentProvider {
         }
 
         // Check if the caller has access to private app directories.
-        if (isUidAllowedAccessToDataOrObbPathForFuse(mCallingIdentity.get().uid, filePath)) {
+        if (isUidAllowedAccessToDataOrObbPath(mCallingIdentity.get().uid, filePath)) {
             return true;
         }
 
@@ -9013,6 +9013,7 @@ public class MediaProvider extends ContentProvider {
      * the caller does not have special access.
      */
     private boolean isPrivatePackagePathNotAccessibleByCaller(String path) {
+        path = FileUtils.normalizeAndFilterDefaultIgnorableCodepoints(path);
         // Files under the apps own private directory
         final String appSpecificDir = extractPathOwnerPackageName(path);
 
@@ -9025,7 +9026,7 @@ public class MediaProvider extends ContentProvider {
         if (isExternalMediaDirectory(path)) {
             return false;
         }
-        return !isUidAllowedAccessToDataOrObbPathForFuse(mCallingIdentity.get().uid, path);
+        return !isUidAllowedAccessToDataOrObbPath(mCallingIdentity.get().uid, path);
     }
 
     private boolean shouldBypassDatabaseAndSetDirtyForFuse(int uid, String path) {
@@ -10006,6 +10007,11 @@ public class MediaProvider extends ContentProvider {
 
     @Keep
     public boolean isUidAllowedAccessToDataOrObbPathForFuse(int uid, String path) {
+        return isUidAllowedAccessToDataOrObbPath(uid,
+                FileUtils.normalizeAndFilterDefaultIgnorableCodepoints(path));
+    }
+
+    private boolean isUidAllowedAccessToDataOrObbPath(int uid, String path) {
         final LocalCallingIdentity token =
                 clearLocalCallingIdentity(getCachedCallingIdentityForFuse(uid));
         try {
