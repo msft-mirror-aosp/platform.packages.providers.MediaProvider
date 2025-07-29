6b652db8a6d344de658ab5118c1c1dd419fa0bce
Garvita Jain <garvitajain@google.com>
2025-07-29 10:59:51 +0000

    Store calling package uid of create bulk requests for permission checks
    Use package info of the create bulk request caller to verify if explicit
    user consent is required and granting uri permission on positive action
    in the PermissionActivity.
    
    BUG: 369105011
    Flag: EXEMPT bugfix
    Test: atest PermissionActivityTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:96bb34d4348019b7521530b81a5c565a3aacc884)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:3c3cf3d0f1c6b84b812ce34be1d5060b1e0a731d
    Merged-In: Ib3e1601e15bb5fabceb8c1bb0e081c4724da27e2
    Change-Id: Ib3e1601e15bb5fabceb8c1bb0e081c4724da27e2

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -30,6 +30,7 @@ import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static android.database.Cursor.FIELD_TYPE_BLOB;
 import static android.provider.CloudMediaProviderContract.EXTRA_ASYNC_CONTENT_PROVIDER;
 import static android.provider.CloudMediaProviderContract.METHOD_GET_ASYNC_CONTENT_PROVIDER;
+import static android.provider.MediaStore.EXTRA_CALLING_PACKAGE_UID;
 import static android.provider.MediaStore.EXTRA_IS_STABLE_URIS_ENABLED;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
@@ -7645,6 +7646,7 @@ public class MediaProvider extends ContentProvider {
 
         final Context context = getContext();
         final Intent intent = new Intent(method, null, context, PermissionActivity.class);
+        extras.putInt(EXTRA_CALLING_PACKAGE_UID, getCallingUidOrSelf());
         intent.putExtras(extras);
         final ActivityOptions options = ActivityOptions.makeBasic();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
